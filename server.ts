b6885fa238abef6dd7b1f27a1eb5bb496ab03e4d
i'mfebri <iamfebriadji@gmail.com>
2021-04-07 01:10:04 +0700

    menambahkan script express urlencoded untuk meng-handle body request

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import session from 'express-session';
-import { config as dotenv } from 'dotenv'
+import { config as dotenv } from 'dotenv';
+import passport from 'passport';
 
 import router from './routes/index';
-import runMongo from './config/database'
+import runMongo from './config/database';
+import passportInit from './middleware/passport';
 
 const 
   app = express()
@@ -13,7 +15,15 @@ app.use(session({
   secret: String(process.env.SESSION_SECRET),
   resave: false,
   saveUninitialized: true
-}))
+}));
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passportInit()
 
 dotenv({ path: './.env' })
 app.use(router); // semua router
