0730172963e02b56b654ec68bb6d8a97638f2c55
i'mfebri <iamfebriadji@gmail.com>
2021-04-07 01:15:13 +0700

    mengubah struktur kode dari class menjadi export function biasa

--- controllers/register.ts
+++ controllers/register.ts
@@ -1,23 +1,18 @@
 import Users from '../models/users';
 
-class RegisterRoute {
-  registerPage = async (req: any, res: any, next: any) => {
-    try {
-      const result = await Users.find().sort({ createdAt: - 1 });
+export const registerPage = async (req:any, res:any, next:any) => {
+  try {
+    const result = await Users.find().sort({ createdAt: - 1 });
 
-      if (!result) res.status(204).json({
-        status: 'success',
-        code: 204,
-        message: 'berhasil mengirim request, tetapi data masih kosong'
-      });
+    if (!result) res.status(204).json({
+      status: 'success',
+      code: 204,
+      message: 'berhasil mengirim request, tetapi data masih kosong'
+    });
 
-      res.status(200).json(result);
-    }
-    catch(err) {
-      console.log(err);
-    }
+    res.status(200).json(result);
+  }
+  catch (err) {
+    console.log(err);
   }
 }
-
-const register = new RegisterRoute;
-export default register;
