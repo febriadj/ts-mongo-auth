f1346928b40657418a0ba26ceac1a08e29f357d2
i'mfebri <iamfebriadji@gmail.com>
2021-04-07 18:32:09 +0700

    update controllers login dan register

--- controllers/login.ts
+++ controllers/login.ts
@@ -1,5 +1,10 @@
 export const loginPage = async (req:any, res:any, next:any) => {
   try {
+    // kondisi jika pengguna sudah login
+    if (req.session.passport) {
+      return res.status(301).redirect('/');
+    }
+
     await res.status(200).render('login', {
       message: undefined
     })
@@ -11,7 +16,8 @@ export const loginPage = async (req:any, res:any, next:any) => {
 
 export const loginPost = async (req:any, res:any, next:any) => {
   try {
-    await res.status(301).redirect('/');
+    // untuk proses login. semua scriptnya berada di folder middleware/passport.js
+    await res.status(301).redirect('/'); // redirect ke halaman dashboard jika sudah berhasil login
   }
   catch(err) {
     console.log(err);
--- controllers/register.ts
+++ controllers/register.ts
@@ -2,6 +2,11 @@ import Users from '../models/users';
 
 export const registerPage = async (req:any, res:any, next:any) => {
   try {
+    // kondisi jika pengguna sudah login
+    if (req.session.passport) {
+      return res.status(301).redirect('/');
+    }
+
     res.status(200).render('register', {
       message: undefined
     })
@@ -15,30 +20,41 @@ export const registerPost = async (req:any, res:any, next:any) => {
   try {
     const { username, email, password, confirmPassword } = req.body;
     
-    // regex script
+    // kondisi jika pengguna sudah login
+    if (req.session.passport) {
+      return res.status(301).redirect('/');
+    }
+
+    // menambahkan script regex
     const 
       usernameRegex = /[A-Z0-9_-]{8,24}/i
     , emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     , passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]){8,24}/
 
+    // kondisi jika username bernilai false
+    // username hanya boleh diisi dengan huruf kecil, besar, angka, underscore, dan tanda penghubung
     if (usernameRegex.test(username) == false) {
       return res.status(400).render('register', {
         message: 'Username Hanya Boleh Menggunakan Huruf Angka Underscore dan Tanda Penghubung'
       })
     }
 
+    // kondisi jika alamat email tidak valid
     if (emailRegex.test(email) == false) {
       return res.status(400).render('register', {
         message: 'Email Tidak Valid'
       })
     }
 
+    // kondisi jika password bernilai false
+    // password harus diisi dengan gabungan huruf kecil, besar, angka, dan simbol
     if (passwordRegex.test(password) == false) {
       return res.status(400).render('register', {
         message: 'Panjang Password Harus Lebih Dari 8 dan Kurang Dari 24, Harus Berupa Huruf Kecil, Huruf Besar, Angka, Dan Simbol'
       })
     }
 
+    // kondisi jika inputan password dan konfirmasi password tidak sesuai
     if (password !== confirmPassword) {
       return res.status(400).render({
         message: 'Password Tidak Sessuai'
@@ -49,11 +65,7 @@ export const registerPost = async (req:any, res:any, next:any) => {
     await newUsers.save((err, result) => {
       if (err) throw err;
 
-      res.status(200).json({
-        status: 'success',
-        code: 200,
-        message: 'akun berhasil dibuat'
-      })
+      res.status(301).redirect('/login'); // redirect ke halaman login jika sudah berhasil membuat akun
     })
   }
   catch(err) {
