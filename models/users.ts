9aad987130885a0f652682caafdb6a575225d75f
i'mfebri <iamfebriadji@gmail.com>
2021-04-08 14:11:32 +0700

    menghilangkan validation untuk password

--- models/users.ts
+++ models/users.ts
@@ -3,20 +3,16 @@ import mongoose, { Schema } from 'mongoose';
 const UsersSchema = new Schema({
   username: {
     type: String,
-    validate: /[A-Z0-9_-]/gi,
+    validate: /[A-Z0-9_-]/i,
     unique: true,
-    required: true
   },
   email: {
     type: String,
     validate: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     unique: true,
-    required: true
   },
   password: {
     type: String,
-    validate: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]){8,24}/,
-    required: true
   }
 }, { 
   timestamps: true 
